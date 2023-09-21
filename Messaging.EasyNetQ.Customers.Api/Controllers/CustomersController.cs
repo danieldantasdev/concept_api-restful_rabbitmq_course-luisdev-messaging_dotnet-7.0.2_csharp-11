56e9ca9f76ed41a7f9c9f2554d9cf3b647b7cb2d
danieldantasdev <contatolobinhodev@gmail.com>
2023-09-21 12:10:47 -0300

    feat: add publishers and subscribers with EasyNetQ

---
+++ Messaging.EasyNetQ.Customers.Api/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using Messaging.EasyNetQ.Customers.Api.Bus;
+using Messaging.EasyNetQ.Customers.Api.Models;
+using Messaging.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Messaging.EasyNetQ.Customers.Api.Controllers
+{
+    [ApiController]
+    [Route("api/customers")]
+	public class CustomersController : ControllerBase
+	{
+        const string ROUTING_KEY = "customer-created";
+        private readonly IBusService _bus;
+
+        public CustomersController(IBusService bus)
+        {
+            _bus = bus;
+        }
+
+        [HttpPost]
+        public IActionResult Post(CustomerInputModel model)
+        {
+            var @event = new CustomerCreated(model.Id, model.FullName, model.Email, model.PhoneNumber, model.BirthDate);
+
+            _bus.Publish(ROUTING_KEY, @event);
+
+            return NoContent();
+        }
+	}
+}
+
