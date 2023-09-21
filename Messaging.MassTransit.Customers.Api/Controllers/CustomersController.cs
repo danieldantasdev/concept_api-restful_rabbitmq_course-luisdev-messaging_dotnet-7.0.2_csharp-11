5b9d7033578ff9e2c2b29c869cf86bfe81feea85
danieldantasdev <contatolobinhodev@gmail.com>
2023-09-21 12:26:24 -0300

    feat: add publishers and subscribers with massTransit

---
+++ Messaging.MassTransit.Customers.Api/Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using Messaging.MassTransit.Customers.Api.Bus;
+using Messaging.MassTransit.Customers.Api.Models;
+using Messaging.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Messaging.MassTransit.Customers.Api.Controllers
+{
+    [ApiController]
+    [Route("api/customers")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly IBusService _bus;
+
+        public CustomersController(IBusService bus)
+        {
+            _bus = bus;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CustomerInputModel model)
+        {
+            var @event = new CustomerCreated(model.Id, model.FullName, model.Email, model.PhoneNumber, model.BirthDate);
+
+            await _bus.Publish(@event);
+
+            return NoContent();
+        }
+    }
+}
+
